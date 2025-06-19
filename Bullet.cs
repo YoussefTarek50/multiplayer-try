b42b4cdbbfd256f216d001c06d96e0a2578e42ec
YoussefTarek50 <youssef.tarek50@gmail.com>
2025-06-19 12:41:12 +0300

    Initial Commit

---
+++ Bullet.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class Bullet : CharacterBody2D
+{
+	public const float Speed = 500.0f;
+	public const float JumpVelocity = -400.0f;
+
+	private Vector2 direction = new Vector2();
+
+	public override void _Ready()
+	{
+		direction = new Vector2(1, 0).Rotated(Rotation);
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		velocity = Speed * direction; 
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	[Export]
+	public PackedScene Bullet;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		if (Input.IsActionJustPressed("fire")) {
+			Node2D b = Bullet.Instantiate<Node2D>();
+			b.RotationDegrees = GetNode<Node2D>("GunRotation").RotationDegrees;
+			b.GlobalPosition = GetNode<Node2D>("GunRotation/BulletSpawn").GlobalPosition;
+			GetTree().Root.AddChild(b);
+		}
+
+
+		GetNode<Node2D>("GunRotation").LookAt(GetViewport().GetMousePosition());
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
